16f1e07d299e39117b597bf574cad11b2dc4bfbf
Jess <jforster@hubspot.com>
2022-08-30 10:23:09 +1000

    Testing

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,21 @@
 import Head from 'next/head'
 import Header from '@components/Header'
 import Footer from '@components/Footer'
-
 export default function Home() {
   return (
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
+        <title>Next.js Starter!!!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!!!" />
+        <Header title="Welcome to my app!" />
         <p className="description">
-          Get starteded by editing <code>pages/index.js</code>
+          Get started by editing <code>pages/index.js</code>
         </p>
       </main>
-
       <Footer />
     </div>
   )
