d03924d0c0983572f5b3f4f07bf37101da88700c
Jess <jforster@hubspot.com>
2022-08-30 10:24:04 +1000

    Testing

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ export default function Home() {
     <div className="container">
       <Head>
         <title>Next.js Starter!</title>
-        <title>Next.js Starter!!!</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
